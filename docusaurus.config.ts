6945ecb21998a6a918e5d229f9b59bfa728d0228
BarryDong <donghao@bwcj.com>
2024-07-28 14:55:47 +0800

    创建 “框架”navbar & “小程序”sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,24 +61,16 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: `Barry's Site`,
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        { type: 'docSidebar', sidebarId: 'framework', position: 'left', label: '框架' },
+        // { type: 'docSidebar', sidebarId: 'tutorialSidebar', position: 'left', label: 'Tutorial' },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        { href: 'https://github.com/facebook/docusaurus', label: 'GitHub', position: 'right' },
       ],
     },
     footer: {
