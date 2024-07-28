3422dc2e310423e29f9ea7acaae11252991bce56
BarryDong <donghao@bwcj.com>
2024-07-28 14:29:42 +0800

    修改基本配置，尝试发布到 Github Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,23 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: `Barry's Site`,
+  tagline: '纸上得来终觉浅，绝知此事要躬行',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://donghao-club.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'donghao-club', // Usually your GitHub org/user name.
+  projectName: 'donghao-club.github.io', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
